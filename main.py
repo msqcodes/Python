1308580209bdd3c03b6336a77dc8a3f0602fe4d2
msqcodes <ceo.msq@gmail.com>
2026-06-21 21:01:32 +0530

    created a main.py file which has my first python project that is expense tracker

---
+++ main.py
@@ -0,0 +1,9 @@
+item = input("enter item: ")
+category = input("enter category: ")
+price = int(input("enter price: "))
+
+print("----------------------------")
+print("MSQ EXPENSE TRACKER")
+print("----------------------------")
+print("\n")
+print("Item     :",item)
