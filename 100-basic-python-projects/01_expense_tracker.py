47adb999dc842e8efc419803496c5fe2b08c3970
Muhammad Sameer Qadri <ceo.msq@gmail.com>
2026-06-28 19:50:17 +0530

    Implement expense input and display feature
    Added basic functionality to input and display expenses.

--- 100-basic-python-projects/01_expense_tracker.py
+++ 100-basic-python-projects/01_expense_tracker.py
@@ -1,1 +1,13 @@
-dfd
+#python project 01 : Expense tracker 
+
+item = input("enter item: ")
+category = input("enter category: ")
+price = int(input("enter price: "))
+
+print("----------------------------")
+print("MSQ EXPENSE TRACKER")
+print("----------------------------")
+print("\n")
+print("Item     :",item)
+print("Category :",category)
+print("Price    :",price)
